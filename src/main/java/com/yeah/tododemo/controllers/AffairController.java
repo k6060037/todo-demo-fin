b7a260c16b3d835ac94a1c8b265e48372c2ff4ad
Mikhail Lukashenko <k6060037@yandex.ru>
2021-02-09 16:34:33 +0300

    first commit again

--- src/main/java/com/yeah/tododemo/controllers/AffairController.java
+++ src/main/java/com/yeah/tododemo/controllers/AffairController.java
@@ -1,15 +1,13 @@
 package com.yeah.tododemo.controllers;
 
 import com.yeah.tododemo.entity.Affair;
-import com.yeah.tododemo.entity.DelAffair;
 import com.yeah.tododemo.repository.AffairRepository;
-import com.yeah.tododemo.repository.DelAffairRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -17,7 +15,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class AffairController {
     private final AffairRepository affairRepository;
-    private final DelAffairRepository delAffairRepository;
 
     @GetMapping(value = "affair")
     public Affair getAffair(@RequestParam(name = "id") UUID id) {
@@ -44,18 +41,8 @@ public class AffairController {
     public Affair copyAffair(@RequestParam(name = "id") UUID id) {
         Affair affair = affairRepository.findById(id).orElseThrow();
         affairRepository.delete(affair);
-        DelAffair delAffair = new DelAffair();
-        delAffair.setAffairDescription(affair.getAffairDescription());
-        delAffair.setAffairPriority(affair.getAffairPriority());
-        delAffair.setId(affair.getId());
-        delAffairRepository.save(delAffair);
         return affair;
     }
 
-    @GetMapping(value = "deleted_affairs")
-    public List<DelAffair> getAllDelAffairs(){
-        return delAffairRepository.findAll();
-
-    }
 
 }
